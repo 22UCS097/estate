4fb6ea32a4fd0be4d49957277180e9508493d0d2
22UCS097 <ravikumar628763@gmail.com>
2025-03-11 14:49:31 +0530

    create signin  api route

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
    server:{
-    proxy:{
+     proxy:{
       '/api':{
         target: 'http://localhost:3000',
         secure:false,
