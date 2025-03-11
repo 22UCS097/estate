441412a6f11aa0a3667efc30e2a42873b3bd3bee
22UCS097 <ravikumar628763@gmail.com>
2025-03-11 14:54:52 +0530

    resolution of errorHandler function as middelware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export  const signup=async(req,res,next)=>{
@@ -22,7 +23,7 @@ export const signin =async (req,res,next)=>{
         const validUser=await User.findOne({email});
          if(!validUser) return next(errorHandler(404,'user Not Found'));
          const validPassword=bcryptjs.compareSync(password,validUser.password);
-         if(!validPassword) return next(errorHandler(401,'Wrong credencial!'));
+         if(!validPassword) return  next(errorHandler(401,'Wrong credencial!'));
           const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET);
           
           const {password:pass, ...rest}=validUser._doc;
