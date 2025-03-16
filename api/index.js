92b14fe8e6c94a1724df3b4a64ed2268da31c789
22UCS097 <ravikumar628763@gmail.com>
2025-03-16 16:13:23 +0530

    create listing route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.router.js'
 import authRouter from "./routes/auth.route.js"
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -29,6 +30,8 @@ app.listen(3000,()=>{
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
+
 
 //middelware:
 app.use((err,req ,res , next)=>{
