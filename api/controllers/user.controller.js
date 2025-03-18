b8b9994dc1ad17aaf52cc4414a095d3054c258f8
22UCS097 <ravikumar628763@gmail.com>
2025-03-18 15:44:31 +0530

    crate get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from '../models/user.model.js'
+import Listing from "../models/listing.model.js";
 
  export const test=(req,res)=>{
     res.json({
@@ -43,5 +44,22 @@ import User from '../models/user.model.js'
            } catch (error) {
             next(error);  
            }
+       }
+
+       export const getUSerListing= async (req,res,next)=>{
+          if(req.user.id === req.params.id){
+             try {
+               const listing = await Listing.find({userRef:req.params.id});
+               res.status(200).json(listing);
+               
+             } catch (error) {
+               next(error);
+             }
+
+          }else{
+             return next(errorHandler(401,'you can only view your listing'));
+
+          }
+
        }
  
