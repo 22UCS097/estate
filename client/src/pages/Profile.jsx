c25bce1b94ed65e58a6991e2476239acc15a41de
22UCS097 <ravikumar628763@gmail.com>
2025-03-18 17:04:56 +0530

    acomplete delte user listing  functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -119,6 +119,25 @@ export default function Profile(){
         }
        }
 
+       const handleListingDelete=async(listingId)=>{
+           try {
+             const res=await fetch(`/api/listing/delete/${listingId}`,{
+                  method:'DELETE',
+
+             });
+
+             const data=await res.json();
+             if(data.success===false){
+               console.log(data.message);
+               return;
+             }
+             setUserListing((prev)=>prev.filter((listing)=>listing._id!==listingId));
+            
+           } catch (error) {
+            console.log(error.message);
+           }
+       }
+
        const handleShowListing= async ()=>{
          try {
              setShowListingError(false);
@@ -209,7 +228,7 @@ export default function Profile(){
                      <p >{listing.name}</p>
                     </Link>
                     <div className="flex flex-col items-center">
-                      <button className='text-red-700 uppercase'>Delete</button>
+                      <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                       <button className='text-green-700 uppercase'>Edit</button>
                     </div>
                     </div>
