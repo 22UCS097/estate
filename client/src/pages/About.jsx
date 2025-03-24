0ec2103efbae89a482198519d02fdffa4b03d132
22UCS097 <ravikumar628763@gmail.com>
2025-03-24 22:08:48 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 export default function About() {
   return (
     <div>
+       <h1 className='text-3xl font-bold mb-4 text-slate-800'> About Esate </h1>
+        <p>
+        An estate refers to a large area of land, property, or assets owned by an individual,
+         family, or organization. It can include residential, commercial, or agricultural land.
+          Estates are often inherited or managed through legal frameworks. In real estate, 
+          an estate may refer to different types of ownership, such as freehold or leasehold. 
+          Large estates may have multiple buildings, gardens, and facilities. Estate planning involves managing assets for future generations.
+           Some estates are historical landmarks or luxury properties. The value of an estate depends on location, size, and market trends.
+            Many estates are governed by local property laws and regulations. Proper management ensures the estate remains profitable and well-maintained.
+        </p>
       
     </div>
   )
